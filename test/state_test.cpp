7a4a515cf5b66dde87c35565e7c847d9ec42e4f9
albe873 <93786475+albe873@users.noreply.github.com>
2025-04-08 21:32:44 +0200

    Update stateTest

--- test/state_test.cpp
+++ test/state_test.cpp
@@ -1,12 +1,51 @@
 #include <gtest/gtest.h>
-
 #include <state.h>
 
-//#include "../src/state.cpp"
+TEST(StateTest, placebo) {
+    std::cout << "placebo test" << std::endl;
+    std::string expected = 
+     R"(0 0 0 B B B 0 0 0 
+0 0 0 0 W 0 0 0 0 
+B 0 0 0 W 0 0 0 B 
+B B W W K W W B B 
+B 0 0 0 W 0 0 0 B 
+0 0 0 0 W 0 0 0 0 
+0 0 0 0 B 0 0 0 0 
+0 0 0 B B B 0 0 0 
+)";
+
+    std::string expected2 = 
+     R"(0 0 0 B B B 0 0 0 
+0 0 0 0 W 0 0 0 0 
+B 0 0 0 W 0 0 0 B 
+B B W W K W W B B 
+B 0 0 0 W 0 0 0 B 
+0 0 0 0 W 0 0 0 0 
+0 0 0 0 B 0 0 0 0 
+0 0 0 B B B 0 0 0 
+)";
+    ASSERT_TRUE(expected.compare(expected2) == 0); 
+}
 
 TEST(StateTest, InitialState) {
+    std::cout << "Initial state test" << std::endl;
     State state = State();
-    std::string expected = "";
+    std::string expected = 
+     R"(0 0 0 B B B 0 0 0 
+0 0 0 0 B 0 0 0 0 
+0 0 0 0 W 0 0 0 0 
+B 0 0 0 W 0 0 0 B 
+B B W W K W W B B 
+B 0 0 0 W 0 0 0 B 
+0 0 0 0 W 0 0 0 0 
+0 0 0 0 B 0 0 0 0 
+0 0 0 B B B 0 0 0 
+)";
+
+    std::cout << "state board toString\n" << std::endl;
+    std::cout << state.boardString() << std::endl;
+    std::cout << "expected board toString\n" << std::endl;
+    std::cout << expected << std::endl;
     ASSERT_TRUE(expected.compare(state.boardString()) == 0);
 }
 
